d32706aeb173fffb1c5cc8c1738a704530da35c4
acoost1c <nia.io.nia.work@gmail.com>
2024-12-02 23:39:29 +0700

    I exported the template for the mod. All of these are empty.

---
+++ src/client/kotlin/com/nikita/guy/NikitaGuyClient.kt
@@ -0,0 +1,9 @@
+package com.nikita.guy
+
+import net.fabricmc.api.ClientModInitializer
+
+object NikitaGuyClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
